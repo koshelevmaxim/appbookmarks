178227bdaaf97dfb88a962fd1ff4a1e80d5581a2
Maxim Koshelev <koshelevmaxim99@gmail.com>
2025-02-13 13:39:38 +0300

    Ссылочный тип map

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ Menu:
 		case 1:
 			printBookmarks(bookmarks)
 		case 2:
-			bookmarks = addBookmark(bookmarks)
+			addBookmark(bookmarks)
 		case 3:
-			bookmarks = deleteBookmark(bookmarks)
+			deleteBookmark(bookmarks)
 		case 4:
 			break Menu
 		}
@@ -45,7 +45,7 @@ func printBookmarks(bookmarks bookmarkMap) {
 
 }
 
-func addBookmark(bookmarks bookmarkMap) bookmarkMap {
+func addBookmark(bookmarks bookmarkMap) {
 	var newBookmarkKey string
 	var newBookmarkValue string
 	fmt.Println("Введите название: ")
@@ -53,13 +53,11 @@ func addBookmark(bookmarks bookmarkMap) bookmarkMap {
 	fmt.Println("Введите ссылку: ")
 	fmt.Scan(&newBookmarkValue)
 	bookmarks[newBookmarkKey] = newBookmarkValue
-	return bookmarks
 }
 
-func deleteBookmark(bookmarks bookmarkMap) bookmarkMap {
+func deleteBookmark(bookmarks bookmarkMap) {
 	var bookmarkToDelete string
 	fmt.Println("Введите название: ")
 	fmt.Scan(&bookmarkToDelete)
 	delete(bookmarks, bookmarkToDelete)
-	return bookmarks
 }
